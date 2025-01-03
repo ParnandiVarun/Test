e6bfeb44c1971b31269641cbb1014b2cfeae2429
Parnandi_ Sricharan_21008490 <parnandivarun@gmail.com>
2025-01-03 23:47:22 +0530

    Adding 2DArray Problems

--- 2DArray_Problem2.js
+++ 2DArray_Problem2.js
@@ -15,24 +15,3 @@ for (let i = 0; i < N; i++) {
 
   console.log(sum);
 }
-
-let arr = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-var value = "";
-let n = 3;
-let m = 3;
-for (let i = n - 1; i >= 0; i--) {
-  value += arr[i][0] + " ";
-}
-for (let j = 1; j < m; j++) {
-  value += arr[0][j] + " ";
-}
-for (let k = 1; k < n; k++) {
-  value += arr[k][2] + " ";
-}
-value += arr[n - 1][n - 2] + " ";
-
-console.log(value);
